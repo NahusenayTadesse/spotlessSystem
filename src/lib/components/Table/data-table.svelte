5a4038ea9a609ec497e9026eb93125ba85713f66
NahusenayTadesse <124673344+NahusenayTadesse@users.noreply.github.com>
2026-01-07 17:04:22 +0300

    Tables have been made resizable

--- src/lib/components/Table/data-table.svelte
+++ src/lib/components/Table/data-table.svelte
@@ -34,6 +34,9 @@
 	import * as Table from '$lib/components/ui/table/index.js';
 	import { Button } from '$lib/components/ui/button/index.js';
 	import { ChevronDownIcon, Frown } from '@lucide/svelte';
+	import * as Resizable from '$lib/components/ui/resizable/index.js';
+	import ResizableHandle from '../ui/resizable/resizable-handle.svelte';
+	import { isMobile } from '$lib/global.svelte';
 
 	let pagination = $state<PaginationState>({ pageIndex: 0, pageSize: 30 });
 	let columnFilters = $state<ColumnFiltersState>([]);
@@ -129,138 +132,160 @@
 </script>
 
 <!-- min-h-0 is required for flex-child overflow -->
-<ScrollArea
-	class="lg:max-w-8xl w-full rounded-lg bg-white p-2 lg:w-fit lg:min-w-2xl  dark:bg-gray-950"
+
+<Resizable.PaneGroup
+	direction="horizontal"
+	class="lg:max-w-8xl flex w-full min-w-full gap-0 lg:w-fit lg:min-w-2xl "
 >
-	<div class="flex min-w-full flex-col gap-2 rounded-md border-0 px-1">
-		{#if search}
-			<div class="py-4">
-				<!-- <Filters
+	<Resizable.Pane
+		defaultSize={isMobile()
+			? 100
+			: table.getAllColumns().filter((col) => col.getIsVisible()).length * 15}
+		class="bg-white p-2 dark:bg-gray-950"
+	>
+		<ScrollArea orientation="vertical" class="w-full rounded-lg p-2">
+			<div class="flex min-w-full flex-col gap-2 rounded-md border-0 px-1">
+				{#if search}
+					<div class="py-4">
+						<!-- <Filters
       schema={filterSchema}
       filters={columnFilters}
       onChange={f => (columnFilters = f)}
     /> -->
-			</div>
-			<div class="sticky top-0 z-20 flex flex-row items-center gap-4 bg-background">
-				<Input
-					type="search"
-					placeholder="Search Table..."
-					class="w-2/3"
-					bind:value={globalFilter}
-					oninput={() => table.setGlobalFilter(globalFilter)}
-				/>
+					</div>
+					<div class="sticky top-0 z-20 flex flex-row flex-wrap items-center gap-4 bg-background">
+						<Input
+							type="search"
+							placeholder="Search Table..."
+							class="w-2/3"
+							bind:value={globalFilter}
+							oninput={() => table.setGlobalFilter(globalFilter)}
+						/>
+						<DropdownMenu.Root>
+							<DropdownMenu.Trigger>
+								{#snippet child({ props })}
+									<Button {...props} variant="outline" class="ml-auto"
+										>Columns <ChevronDownIcon class="size-5" />
+									</Button>
+								{/snippet}
+							</DropdownMenu.Trigger>
+							<DropdownMenu.Content align="end">
+								{#each table.getAllColumns().filter((col) => col.getCanHide()) as column (column)}
+									<DropdownMenu.CheckboxItem
+										class="capitalize"
+										bind:checked={() => column.getIsVisible(), (v) => column.toggleVisibility(!!v)}
+									>
+										{column.id.replace(/([a-z])([A-Z])/g, '$1 $2')}
+									</DropdownMenu.CheckboxItem>
+								{/each}
+							</DropdownMenu.Content>
+						</DropdownMenu.Root>
 
-				<DropdownMenu.Root>
-					<DropdownMenu.Trigger>
-						{#snippet child({ props })}
-							<Button {...props} variant="outline" class="ml-auto"
-								>Columns <ChevronDownIcon class="size-5" />
-							</Button>
-						{/snippet}
-					</DropdownMenu.Trigger>
-					<DropdownMenu.Content align="end">
-						{#each table.getAllColumns().filter((col) => col.getCanHide()) as column (column)}
-							<DropdownMenu.CheckboxItem
-								class="capitalize"
-								bind:checked={() => column.getIsVisible(), (v) => column.toggleVisibility(!!v)}
-							>
-								{column.id.replace(/([a-z])([A-Z])/g, '$1 $2')}
-							</DropdownMenu.CheckboxItem>
-						{/each}
-					</DropdownMenu.Content>
-				</DropdownMenu.Root>
+						<DropdownMenu.Root>
+							<DropdownMenu.Trigger>
+								{#snippet child({ props })}
+									<Button {...props} variant="outline" class="ml-auto"
+										>Pages <ChevronDownIcon class="size-5" />
+									</Button>
+								{/snippet}
+							</DropdownMenu.Trigger>
+							<DropdownMenu.Content align="end">
+								{#each getTableBreakpoints(data) as column (column)}
+									<DropdownMenu.Item
+										class="capitalize"
+										onclick={() => {
+											table.setPageSize(column);
+										}}
+									>
+										{column}
+									</DropdownMenu.Item>
+								{/each}
+							</DropdownMenu.Content>
+						</DropdownMenu.Root>
+						<Pdf {fileName} tableId="#{uniqueTableId}" {data} />
+					</div>
+				{/if}
+				<div class="max-h-96 rounded-md border">
+					<Table.Root id={uniqueTableId} class="relative max-h-96">
+						<Table.Header>
+							{#each table.getHeaderGroups() as headerGroup (headerGroup.id)}
+								<Table.Row>
+									{#each headerGroup.headers as header, index}
+										<Table.Head
+											colspan={header.colSpan}
+											class="{index === 1
+												? 'sticky left-0 z-10 bg-background'
+												: ''} p-0 pr-2 text-start"
+										>
+											{#if !header.isPlaceholder}
+												<FlexRender
+													content={header.column.columnDef.header}
+													context={header.getContext()}
+												/>
+											{/if}
+										</Table.Head>
+									{/each}
+								</Table.Row>
+							{/each}
+						</Table.Header>
+						<Table.Body>
+							{#each table.getRowModel().rows as row (row.id)}
+								<Table.Row data-state={row.getIsSelected() && 'selected'}>
+									{#each row.getVisibleCells() as cell, index}
+										<Table.Cell
+											class="max-w-[200px] break-words capitalize {index === 1
+												? 'sticky left-0 z-10 bg-background'
+												: ''}"
+										>
+											<FlexRender
+												content={cell.column.columnDef.cell}
+												context={cell.getContext()}
+											/>
+										</Table.Cell>
+									{/each}
+								</Table.Row>
+							{:else}
+								<Table.Row>
+									<Table.Cell colspan={columns.length} class="text-center font-2xl">
+										<div class="flex flex-row items-center justify-center gap-2">
+											<Frown class="animate-bounce" /> Nothing found here.
+										</div>
+									</Table.Cell>
+								</Table.Row>
+							{/each}
+						</Table.Body>
+					</Table.Root>
 
-				<DropdownMenu.Root>
-					<DropdownMenu.Trigger>
-						{#snippet child({ props })}
-							<Button {...props} variant="outline" class="ml-auto"
-								>Pages <ChevronDownIcon class="size-5" />
-							</Button>
-						{/snippet}
-					</DropdownMenu.Trigger>
-					<DropdownMenu.Content align="end">
-						{#each getTableBreakpoints(data) as column (column)}
-							<DropdownMenu.Item
-								class="capitalize"
-								onclick={() => {
-									table.setPageSize(column);
-								}}
+					{#if table.getPageCount() > 1}
+						<div
+							class="absolute -bottom-5 flex w-full items-end justify-end space-x-2 justify-self-center py-4"
+						>
+							<Button
+								variant="outline"
+								size="sm"
+								onclick={() => table.previousPage()}
+								disabled={!table.getCanPreviousPage()}
 							>
-								{column}
-							</DropdownMenu.Item>
-						{/each}
-					</DropdownMenu.Content>
-				</DropdownMenu.Root>
-				<Pdf {fileName} tableId="#{uniqueTableId}" {data} />
-			</div>
-		{/if}
-		<div class="max-h-96 rounded-md border">
-			<Table.Root id={uniqueTableId} class="relative max-h-96">
-				<Table.Header>
-					{#each table.getHeaderGroups() as headerGroup (headerGroup.id)}
-						<Table.Row>
-							{#each headerGroup.headers as header, index}
-								<Table.Head
-									colspan={header.colSpan}
-									class="{index === 1
-										? 'sticky left-0 z-10 bg-background'
-										: ''} p-0 pr-2 text-start"
-								>
-									{#if !header.isPlaceholder}
-										<FlexRender
-											content={header.column.columnDef.header}
-											context={header.getContext()}
-										/>
-									{/if}
-								</Table.Head>
-							{/each}
-						</Table.Row>
-					{/each}
-				</Table.Header>
-				<Table.Body>
-					{#each table.getRowModel().rows as row (row.id)}
-						<Table.Row data-state={row.getIsSelected() && 'selected'}>
-							{#each row.getVisibleCells() as cell, index}
-								<Table.Cell
-									class="capitalize {index === 1 ? 'sticky left-0 z-10 bg-background' : ''}"
-								>
-									<FlexRender content={cell.column.columnDef.cell} context={cell.getContext()} />
-								</Table.Cell>
-							{/each}
-						</Table.Row>
-					{:else}
-						<Table.Row>
-							<Table.Cell colspan={columns.length} class="text-center font-2xl">
-								<div class="flex flex-row items-center justify-center gap-2">
-									<Frown class="animate-bounce" /> Nothing found here.
-								</div>
-							</Table.Cell>
-						</Table.Row>
-					{/each}
-				</Table.Body>
-			</Table.Root>
-			{#if table.getPageCount() > 1}
-				<div
-					class="absolute -bottom-5 flex w-full items-end justify-end space-x-2 justify-self-center py-4"
-				>
-					<Button
-						variant="outline"
-						size="sm"
-						onclick={() => table.previousPage()}
-						disabled={!table.getCanPreviousPage()}
-					>
-						Previous
-					</Button>
-					<Button
-						variant="outline"
-						size="sm"
-						onclick={() => table.nextPage()}
-						disabled={!table.getCanNextPage()}
-					>
-						Next
-					</Button>
+								Previous
+							</Button>
+							<Button
+								variant="outline"
+								size="sm"
+								onclick={() => table.nextPage()}
+								disabled={!table.getCanNextPage()}
+							>
+								Next
+							</Button>
+						</div>
+					{/if}
 				</div>
-			{/if}
-		</div>
-	</div>
-</ScrollArea>
+			</div>
+		</ScrollArea>
+	</Resizable.Pane>
+	<ResizableHandle withHandle />
+	{#if isMobile()}
+		<Resizable.Pane defaultSize={0}></Resizable.Pane>
+	{:else}
+		<Resizable.Pane></Resizable.Pane>
+	{/if}
+</Resizable.PaneGroup>
